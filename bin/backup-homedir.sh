b1492b042b5b5fe84af6ad169497aa252d3c2b1c
Andy Cobaugh <andrewcobaugh@gmail.com>
2017-05-13 13:05:45 -0400

    s/phalenor/acobaugh/g

--- bin/backup-homedir.sh
+++ bin/backup-homedir.sh
@@ -2,6 +2,6 @@
 
 /bin/rsync -avP --no-specials -e "ssh -l phalenor -c arcfour" \
 	--delete-excluded --delete-after \
-	--exclude-from=/home/phalenor/bin/homedir_backup_exclude.txt \
-	/home/phalenor/ \
-	172.30.0.10:/afs/phalengard.com/backups/zirzla/home-phalenor/
+	--exclude-from=$HOME/bin/homedir_backup_exclude.txt \
+	$HOME \
+	172.30.0.10:/afs/phalengard.com/backups/zirzla/home-acobaugh/
