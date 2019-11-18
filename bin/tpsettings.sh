3acaf609a7d7f09bf591d50259953f52b185deb2
Andy Cobaugh <andrew.cobaugh@gmail.com>
2019-11-17 21:32:49 -0500

    tweak settings for thinkpad

--- bin/tpsettings.sh
+++ bin/tpsettings.sh
@@ -13,11 +13,12 @@ fi
 #------------------------------------------------------------
 if [ -d "$TPDEV" ]; then
     echo "Configuring Trackpoint"
-    #echo -n 140 | sudo tee $TPDEV/sensitivity # def 200 on t450s
     #echo -n 65 | sudo tee $TPDEV/speed # def 97 on t450s
     #echo -n 5   | sudo tee $TPDEV/drift_time # def 5 on t450s
-    echo -n 128 | sudo tee $TPDEV/sensitivity # def 200 on t450s
+    echo -n 150 | sudo tee $TPDEV/sensitivity # def 200 on t450s
     echo -n 75 | sudo tee $TPDEV/rate # def 100 on t450s
+    echo -n 1 | sudo tee $TPDEV/inertia
+    echo -n 255 | sudo tee $TPDEV/resolution
 else
     echo "Couldn't find trackpoint device $TPDEV"
 fi
