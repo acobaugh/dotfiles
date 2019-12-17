51607c681bfa8cb34667a24e1b425322556e216c
Andy Cobaugh <andrew.cobaugh@gmail.com>
2019-12-17 03:31:51 +0000

    fix evict-evicted.sh

--- bin/evict-evicted.sh
+++ bin/evict-evicted.sh
@@ -2,5 +2,5 @@
 
 for ns in `kubectl get pods --all-namespaces | grep Evicted | awk '{ print $1 }'| sort | uniq` ; do 
 	echo "# $ns"
-	kdelp -n $ns `kubectl get pods -n $ns | grep Evicted | awk '{ print $1 }'`
+	kubectl delete pod -n $ns `kubectl get pods -n $ns | grep Evicted | awk '{ print $1 }'`
 done
