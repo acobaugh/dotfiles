76f1621e9f6d91a471059728fd32f99278fb11fa
Andy Cobaugh <andrew.cobaugh@gmail.com>
2020-10-01 20:35:46 -0400

    bin: use ldapsearch in group_members.sh

--- bin/group_members.sh
+++ bin/group_members.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-for uid in `getent group $1 | cut -f4 -d: | sed -e 's/,/ /g'` ; do echo -en "$uid\t " ; echo -e "$(getent passwd $uid | cut -f5 -d: )" ; done | sort
+lds="ldapsearch -x -h dirapps.aset.psu.edu -b dc=psu,dc=edu -LLL"
+for uid in `$lds cn=$1 memberuid | cut -f2 -d: | sed -e 's/,/ /g'` ; do echo -en "$uid\t " ; echo -e "$($lds uid=$uid | grep -i displayname | cut -f2 -d:)" ; done | sort
